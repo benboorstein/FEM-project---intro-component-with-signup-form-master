af44b92d384b9add58605b7691073a49a33dc970
Ben <benboorstein@gmail.com>
2021-08-24 10:58:45 -0400

    html, css, js changes

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+document.querySelector('.free-trial-btn').addEventListener('click', function() {
+    document.querySelector('form').classList.add('submitted')
+
+    if (document.querySelector('.form-input').value == false) { // QQQ: This seems to work. Is this how I do what I'm trying to do on this line?
+        document.querySelector('.p').classList.remove('is-hidden')
+    } else {
+        document.querySelector('.p').classList.add('is-hidden')
+    }
+})
+
+// QQQ: And why is the paragraph only showing up under the top input in the UI and none of the other inputs?
