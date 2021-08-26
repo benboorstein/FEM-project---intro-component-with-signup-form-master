f9220797c2f00cf48c710c2f1e66837a02418400
Ben <benboorstein@gmail.com>
2021-08-25 20:23:50 -0400

    js changes.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,19 @@
 document.querySelector('.free-trial-btn').addEventListener('click', function() {
     document.querySelector('form').classList.add('submitted')
 
-    if (document.querySelector('.form-input').value == false) { //// QQQ: This seems to work. Is this how I do what I'm trying to do on this line?
-        document.querySelector('.p').classList.remove('is-hidden')
-    } else {
-        document.querySelector('.p').classList.add('is-hidden')
+    // LEFT OFF HERE
+    const formInputs = document.querySelectorAll('.form-input')
+    const errorPs = document.querySelectorAll('.error-p') 
+
+    for (let i = 0; i < formInputs.length; i++) {
+        for (let j = 0; j < errorPs.length; j++) {
+            if (formInputs[i].value == false) {
+                errorPs[j].classList.remove('is-hidden') 
+            } else {
+                errorPs[j].classList.add('is-hidden')
+                // QQQ: The above line isn't doing what I want it to do...
+                // ...What I want is for the paragraph to become hidden as soon as formInputs[i].value is true/valid.
+            }
+        }
     }
 })
-
-//// QQQ: And why is the paragraph only showing up under the top input in the UI and none of the other inputs?
